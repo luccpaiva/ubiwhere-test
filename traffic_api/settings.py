a6caa4048ac7cb70a69ba1e276414b7a6787d69c
Lucas Paiva <lucaspaiva96@gmail.com>
2026-01-11 00:00:00 +0000

    Docs: updating README for Part1

--- traffic_api/settings.py
+++ traffic_api/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = "traffic_api.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "traffic_api",
+        "USER": "postgres",
+        "PASSWORD": "your_password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
